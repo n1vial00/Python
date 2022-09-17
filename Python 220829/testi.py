9436b78943d392a991ceaa7d9c83e988ff14623d
n1vial00 <n1vial00@students.oamk.fi>
2022-09-18 00:58:21 +0300

    Kek

---
+++ Python 220829/testi.py
@@ -0,0 +1,4 @@
+import numpy as np
+numpy.nanmean(2)
+
+from numpy import nanmean as special_mean_function
---
+++ scripts.py
@@ -0,0 +1,10 @@
+def maximum(x):
+    z = max(x)
+    return z
+#def ordering(x):
+
+#def calcAvg(x):
+t = (6,12,42,1)
+
+print(maximum(t))
+
